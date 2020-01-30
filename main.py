8ff1322c55ac7fc241bf096bf97d9ed711851117
kdh89 <khodang012@gmail.com>
2020-01-30 12:32:26 -0500

    Display player sprite

--- main.py
+++ main.py
@@ -4,13 +4,22 @@ def main():
     # Initialize pygame
     pygame.init()
 
-    # Create screen
+    # Create screen with 1280x720 resolution
     screen = pygame.display.set_mode((1280, 720))
 
     # Title and icon
     pygame.display.set_caption("Boss Battle")
+    # TODO: replace icon image
     icon = pygame.image.load("oubliette.png")
     pygame.display.set_icon(icon)
+
+    # Display player
+    # TODO: replace player image
+    playerImg = pygame.image.load("terror.png")
+    playerX = 640
+    playerY = 360
+    def displayPlayer():
+        screen.blit(playerImg, (playerX, playerY))
 
     # Game loop
     isRunning = True
@@ -20,7 +29,11 @@ def main():
                 isRunning = False
 
         # RGB background
-        screen.fill((255, 255, 255))
+        screen.fill((255, 255, 0))
+
+        displayPlayer()
+
+        # WARNING: update() function below does not work for python3.7 on MacOS Catalina unless using anaconda3
         pygame.display.update()
 
 if __name__ == "__main__":
