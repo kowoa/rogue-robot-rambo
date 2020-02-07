f80692d5ca1cd576cf540e2e85fb31b92e550452
mli28 <mli28@drexel.edu>
2020-02-07 14:59:47 -0500

    Add wasd controls

--- src/player.py
+++ src/player.py
@@ -8,17 +8,19 @@ class Player(Entity):
 
     def doKeyState(self):
         keystate = pygame.key.get_pressed()
-        if keystate[pygame.K_LEFT]:
-            self.physics.vel[0] = -5
-            #self.shoot()
-        elif keystate[pygame.K_RIGHT]:
-            self.physics.vel[0] = 5
-            #self.shoot()
-        elif keystate[pygame.K_UP]:
+
+        if keystate[pygame.K_UP] or keystate[pygame.K_w]:
             self.physics.vel[1] = -5
             #self.shoot()
-        elif keystate[pygame.K_DOWN]:
+        elif keystate[pygame.K_DOWN] or keystate[pygame.K_s]:
             self.physics.vel[1] = 5
+            #self.shoot()
+
+        elif keystate[pygame.K_LEFT] or keystate[pygame.K_a]:
+            self.physics.vel[0] = -5
+            #self.shoot()
+        elif keystate[pygame.K_RIGHT] or keystate[pygame.K_d]:
+            self.physics.vel[0] = 5
             #self.shoot()
         else:
             self.physics.vel = np.array([0,0])
