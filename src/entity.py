28644ce7c59f25a2ec457c6303bf73f2f89b6a18
mli28 <mli28@drexel.edu>
2020-02-11 12:53:42 -0500

    Add boundary detection

--- src/entity.py
+++ src/entity.py
@@ -5,11 +5,25 @@ import numpy as np
 class Entity():
 
     # Takes int mass and numpy arrays [x,y] pos, vel, and acc
-    def __init__(self, mass=int(), pos=np.array([0, 0]), vel=np.array([0, 0]), acc=np.array([0, 0])):
+    def __init__(self, size = int(), mass=int(), pos=np.array([0, 0]), vel=np.array([0, 0]), acc=np.array([0, 0])):
+        self.size = size
         self.mass = mass
         self.pos = pos
         self.vel = vel
         self.acc = acc
+
+
+    def detectBounds(self):
+        if self.pos[0] < 0:
+            self.pos[0] = 0
+        if self.pos[0] > 1280-32:
+            self.pos[0] = 1280-32
+        if self.pos[1] < 0:
+            self.pos[1] = 0
+        if self.pos[1] > 720-32:
+            self.pos[1] = 720-32
+
+
 
     # Physics Methods
     def getForce(self):
@@ -18,6 +32,7 @@ class Entity():
     def applyForce(self, force):
         self.acc = (self.force // self.mass)
 
-    def updatePhysics(self):
+    def updateMovement(self):
+        self.detectBounds()
         self.vel += self.acc
         self.pos += self.vel
