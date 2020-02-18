b25ed088dedad10f39ccc15596315a2003597a66
are53 <are53@drexel.edu>
2020-02-17 21:48:55 -0500

    Updated sprites and new spritesheets

--- src/entities.py
+++ src/entities.py
@@ -3,7 +3,7 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
-        self.image = pygame.image.load("resources/sprites/boss.png")
+        self.image = pygame.image.load("resources/sprites/newBoss.png")
         self.rect = self.image.get_rect()
         self.pos = pygame.Vector2((100, 200))
 
@@ -18,5 +18,3 @@ class Player(pygame.sprite.Sprite):
         self.pos += move * (dt/5)
         self.rect.center = self.pos
 
-class Bullets(pygame.sprite.Sprite):
-    def __init__(self):
