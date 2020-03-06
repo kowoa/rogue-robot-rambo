06b80512ab43bc5af44e131b3f5d409c9bf2815a
are53 <are53@drexel.edu>
2020-03-06 14:11:55 -0500

    Scoreboard design added

--- src/constants.py
+++ src/constants.py
@@ -6,9 +6,11 @@ SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
 SCREEN_TITLE = "Boss Battle!"
 SCREEN_ICON = "resources/sprites/oubliette.png"
+SCORE_FILE = "highscore.txt"
 FPS = 60
 
 # Fonts
 FONT_SMALL = pygame.font.SysFont("monospace", 24)
-FONT_MEDIUM = pygame.font.SysFont("monospace", 32)
-FONT_LARGE = pygame.font.SysFont("monospace", 40)
+FONT_MEDIUM = pygame.font.SysFont("monospace", 40)
+FONT_LARGE = pygame.font.SysFont("monospace", 60)
+FONT_XLARGE = pygame.font.SysFont("monospace", 80)
