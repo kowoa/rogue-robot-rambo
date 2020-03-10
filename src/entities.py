e57518d35f8afcc3d1b79eaa20e48d4c17cc7558
are53 <are53@drexel.edu>
2020-03-09 22:01:51 -0400

    More platforms

--- src/entities.py
+++ src/entities.py
@@ -174,7 +174,7 @@ class Gun(pygame.sprite.Sprite):
 class Platform(pygame.sprite.Sprite):
     def __init__(self, x, y):
         super().__init__()
-        self.image = pygame.image.load('resources/sprites/blockade.png')
+        self.image = pygame.image.load('resources/sprites/platform.png')
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
