2ec67d6741bcc4f586feb8ae06eaa281bf2b8977
are53 <are53@drexel.edu>
2020-04-18 16:29:54 -0400

    Testing

---
+++ src/gui.py
@@ -0,0 +1,17 @@
+class Button():
+    def __init__(self, width, height, color, text):
+        self.width = width
+        self.height = height
+        self.color = color
+        self.text = text
+
+    def
+
+class Menu():
+    def __init__(self, background_color = (255, 255, 255), title = 'None', button = 0, button_format = 'None'):
+        self.background_color = background_color
+        self.title = title
+        self.button = button
+        self.button_format = button_format
+
+    def 
